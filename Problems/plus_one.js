4e330643f771b3111805331c89c8db25ee77749e
Lieba Pil <pil.lieba@gmail.com>
2024-04-01 15:01:13 -0700

    problem 3

---
+++ Problems/plus_one.js
@@ -0,0 +1,15 @@
+function plusOne(digits) {
+    let carry = 1; 
+    for (let i = digits.length - 1; i >= 0; i--) {
+        const sum = digits[i] + carry;
+        digits[i] = sum % 10; 
+        carry = Math.floor(sum / 10); 
+        if (carry === 0) break; 
+    }
+    if (carry > 0) {
+        digits.unshift(carry); 
+    }
+    return digits;
+}
+
+
