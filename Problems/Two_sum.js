019e643e47b27f3fd31f65b6b9744ea4a70d27c2
Lieba Pil <pil.lieba@gmail.com>
2024-05-08 15:58:16 -0700

    two sum

---
+++ Problems/Two_sum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        for (let j = (i + 1); j < nums.length; j++) {
+            const num = nums[j];
+
+            const isTarget = num === complement
+            if (isTarget) return [ i, j ];
+        }
+    }
+
+    return [ -1, -1 ];
+};
